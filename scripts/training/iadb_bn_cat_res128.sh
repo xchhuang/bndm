2f8a494ff1abf47a8c35108e75c220fa55d18acb
xchhuang <dominjune@outlook.com>
2024-08-01 14:29:27 -0600

    adding more training scripts

---
+++ scripts/training/iadb_bn_cat_res128.sh
@@ -0,0 +1,10 @@
+
+# gaussian
+python iadb_bn.py --dataset=cat_re128 --res=128 --batch_size=64 --epochs=1000 --train_or_test=train --lr=0.0001 --grad_clip=1.0  --noise_type=gaussian --scheduler_gamma=linear --scheduler_param=1000 --out_channel=3
+
+# gaussianBN
+python iadb_bn.py --dataset=cat_re128 --res=128 --batch_size=64 --epochs=1000 --train_or_test=train --lr=0.0001 --grad_clip=1.0  --noise_type=gaussianBN --scheduler_gamma=sigmoid --scheduler_param=0.2 --out_channel=6
+
+
+
+
